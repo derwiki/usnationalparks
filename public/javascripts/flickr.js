c3ec886b782d6089fcf0090e78b98ba338e8d084
Adam Derewecki <derewecki@gmail.com>
2016-08-07 09:13:38 -0700

    Clean up flickr integration

--- public/javascripts/flickr.js
+++ public/javascripts/flickr.js
@@ -16,32 +16,20 @@ var flickr = {
       },
       function(data) {
         $.each(data.items, function(i, item){
-          var description = flickr.cleanDescription(item.description);
-
           var $img = $("<img/>")
             .attr("src", item.media.m)
             .attr("title", item.title)
             .attr("alt", item.title);
-          var $desc = $("<div/>")
-            .html(description);
-          var $link = $("<a/>")
+          var $link = $("<a class='flickr'/>")
             .attr("href", item.link)
             .html($img);
 
           $link.appendTo("#"+park.underscored_title);
-          $desc.appendTo("#"+park.underscored_title);
+
           if (i === 2) { return false; }
         }.bind(this));
       }
     );
   },
-
-  cleanDescription: function(desc) {
-    var matches = desc.match(/ ?posted a photo:/);
-    matches && matches.forEach(function(junk) {
-      desc = desc.replace(junk, '');
-    });
-    return desc;
-  }
 };
 document.addEventListener("DOMContentLoaded", flickr.init);
